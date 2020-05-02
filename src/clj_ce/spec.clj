8bd60707f92a0cdd6e587145a1d6d72013378a97
Matej Vasek <mvasek@redhat.com>
2020-05-02 02:53:55 +0200

    Add data and extensions to cljspec

--- src/clj_ce/spec.clj
+++ src/clj_ce/spec.clj
@@ -9,10 +9,10 @@
 (defmulti event-version :ce/spec-version)
 (defmethod event-version "0.3" [_]
   (s/keys :req [:ce/id :ce/spec-version :ce/source :ce/type]
-          :opt [:ce/data-content-type :ce/schema-url :ce/subject :ce/data-content-encoding :ce/time]))
+          :opt [:ce/data-content-type :ce/schema-url :ce/subject :ce/data-content-encoding :ce/time :ce/data :ce/extensions]))
 (defmethod event-version "1.0" [_]
   (s/keys :req [:ce/id :ce/spec-version :ce/source :ce/type]
-          :opt [:ce/data-content-type :ce/data-schema :ce/subject :ce/time]))
+          :opt [:ce/data-content-type :ce/data-schema :ce/subject :ce/time :ce/data :ce/extensions]))
 
 
 (s/def :ce/event (s/multi-spec event-version :ce/spec-version))
