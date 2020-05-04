bcdf21a51fabdbd72e16d2abd95c8917a93ac394
Matej Vasek <mvasek@redhat.com>
2020-05-04 05:47:18 +0200

    Update license and readme

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-ce "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Clojure(Script) library designed to handle CloudEvents."
+  :url "https://github.com/matejvasek/clj-ce"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.753"]]
   :global-vars {*warn-on-reflection* true}
